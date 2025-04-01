4c8dbc1e340985aaa2894d323760125e3b36ce71
Salcedo Sanchez Juan David <juandavidsalcedo85@gmail.com>
2025-03-31 22:14:25 -0500

    Update: supabase from auth-helpers to ssr (in progress)

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -2,17 +2,16 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { AuthSignInButtonServer } from "@/app/components/auth/auth-button-server";
-import { getSession } from "../services/auth/get-session";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
-  const session = await getSession()
+  const { data: { session } } = await supabase.auth.getSession()
 
   if (session === null) {
     redirect('/login')
   }
 
-  const { data } = await supabase.from('tasks').select()
+  // const { data } = await supabase.from('tasks').select()
 
   return (
     <div className="flex flex-col bg-gray-900 min-h-screen items-center justify-center text-gray-200">
@@ -20,13 +19,12 @@ export default async function Home() {
       <h1 className="py-5 text-3xl ">
         Acá iran las tareas del usuario con su respectivo grid
       </h1>
-
       <AuthSignInButtonServer />
-      <pre>
+      {/* <pre>
         {
           JSON.stringify(data, null, 2)
         }
-      </pre>
+      </pre> */}
     </div>
   );
 }
