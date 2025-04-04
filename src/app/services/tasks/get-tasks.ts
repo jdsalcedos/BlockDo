1fa7c96dc9feca706818e5352bd6fc73f5957201
Salcedo Sanchez Juan David <juandavidsalcedo85@gmail.com>
2025-04-03 20:29:57 -0500

    better filter for display tasks

--- src/app/services/tasks/get-tasks.ts
+++ src/app/services/tasks/get-tasks.ts
@@ -3,12 +3,15 @@ import { createClient } from "@/app/utils/supabase/server";
 export async function getTaskData() {
   const supabase = await createClient()
 
-  const { data, error } = await supabase.from('tasks').select()
+  const { data: tasks, error } = await supabase
+    .from('tasks')
+    .select('*, user:users(*)') // trae los datos de la tabla tasks y los datos del usuario relacionado
+    .order('created_at', { ascending: false })
 
   if (error) {
     console.error(error)
     return []
   }
 
-  return data || []
+  return tasks || []
 }
